326ddeb1ac7233e52b62e9e9aee17b9deab24465
venadda <meetvenku@hotmail.com>
2021-10-22 18:31:49 +0530

    whatsup structure done

---
+++ whatsup/src/test/java/com/whatsup/whatsup/WhatsupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whatsup.whatsup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
